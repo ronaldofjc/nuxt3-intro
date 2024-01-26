8ca637128b0903833ab991d0e5e3ca27754e6d74
Ronaldo Feitosa <ronaldo.feitosa@mercadolivre.com>
2024-01-26 16:17:47 -0300

    components - shared - icons

---
+++ components/video/Detalhes.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Componentes - Nuxt 3</h1>
+    <p>Autor: Ronaldo Feitosa</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
